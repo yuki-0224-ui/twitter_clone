a9e937b553058c76e9d33a5829d8b916e80fad88
yuki-0224-ui <sannomiya0224@gmail.com>
2024-12-20 22:13:15 +0900

    認証系のルーティング追加

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', TemplateView.as_view(template_name='hello.html')),
+    path('', include('tweets.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
