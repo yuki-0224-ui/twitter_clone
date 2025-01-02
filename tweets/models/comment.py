aa6fff9e01d5de14bca8e37fa0ad56d171380f1b
yuki-0224-ui <sannomiya0224@gmail.com>
2025-01-03 00:38:29 +0900

    モデル追加(コメント、リツイート、いいね)

---
+++ tweets/models/comment.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    tweet = models.ForeignKey(
+        'Tweet',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    parent_comment = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='replies'
+    )
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.user.username} commented on {self.tweet.id}'
