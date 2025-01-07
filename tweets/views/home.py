2b38a42429d49ee34956208f2acad7c6e82506c5
yuki-0224-ui <sannomiya0224@gmail.com>
2025-01-07 14:47:42 +0900

    HomeViewにPOSTメソッドを追加しツイート投稿を実装

--- tweets/views/home.py
+++ tweets/views/home.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView
+from django.shortcuts import redirect
+from django.contrib import messages
 from tweets.models import Tweet
+from tweets.forms import TweetForm
 
 
 class HomeView(ListView):
@@ -23,7 +26,22 @@ class HomeView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['current_tab'] = self.get_tab()
+        context['form'] = TweetForm()
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            messages.success(request, 'ツイートを投稿しました。')
+            return redirect('tweets:home')
+
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+        context['form'] = form
+        return self.render_to_response(context)
 
 
 home = HomeView.as_view()
