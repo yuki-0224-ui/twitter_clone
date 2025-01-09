f681278e153a501c16d7e98d481a19846b799de9
yuki-0224-ui <sannomiya0224@gmail.com>
2025-01-09 12:31:58 +0900

    コメントフォームの作成

--- tweets/forms/__init__.py
+++ tweets/forms/__init__.py
@@ -1,11 +1,13 @@
 from .auth import CustomLoginForm, CustomSignupForm
 from .profile import ProfileEditForm
 from .tweet import TweetForm
+from .comment import CommentForm
 
 __all__ = [
     'CustomLoginForm',
     'CustomSignupForm',
     'ProfileEditForm',
     'TweetForm',
+    'CommentForm',
 
 ]
