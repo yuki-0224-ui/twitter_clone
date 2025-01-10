85f613ed0e9a710b6bb17c7158f2e62e949e7a5f
yuki-0224-ui <sannomiya0224@gmail.com>
2025-01-10 13:28:29 +0900

    homeビューからcurrent_tabコンテキストを削除

--- tweets/views/home.py
+++ tweets/views/home.py
@@ -25,7 +25,6 @@ class HomeView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['current_tab'] = self.get_tab()
         context['form'] = TweetForm()
         return context
 
